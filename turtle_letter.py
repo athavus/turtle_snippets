56421b016d78b98d7389728f0a62c39ff76ba9bd
Miguel Ryan Dantas de Freitas <miguelryanfreitas@gmail.com>
2024-03-23 19:12:17 -0300

    add some letters

--- turtle_letter.py
+++ turtle_letter.py
@@ -31,6 +31,46 @@ def draw_a():
     t.forward(75)
     t.left(75)
 
+# Letra B
+def draw_b():
+    t.left(90)
+    t.forward(150)
+    t.right(90)
+    t.forward(75)
+    t.right(90)
+    t.forward(75)
+    t.right(90)
+    t.forward(75)
+    t.left(90)
+    t.forward(10)
+    t.left(90)
+    t.forward(75)
+    t.right(90)
+    t.forward(65)
+    t.right(90)
+    t.forward(75)
+    t.left(180)
+    t.forward(75)
+
+# Letra C
+def draw_c():
+    t.color("white")
+    t.forward(75)
+    t.left(90)
+    t.forward(150)
+    t.left(90)
+    t.color("black")
+    t.circle(75, 180)
+
+# Letra D
+def draw_d():
+    t.left(90)
+    t.forward(150)
+    t.left(90)
+    t.circle(75, -180)
+    t.pen(pencolor=None)
+    t.forward(75)
+
 # Letra E
 def draw_e():
     t.left(90)
@@ -49,6 +89,34 @@ def draw_e():
     t.forward(75)
     t.left(90)
     t.forward(75)
+
+# Letra F
+def draw_f():
+    t.left(90)
+    t.forward(150)
+    t.right(90)
+    t.forward(75)
+    t.right(180)
+    t.forward(75)
+    t.left(90)
+    t.forward(75)
+    t.left(90)
+    t.forward(75)
+    t.left(180)
+    t.forward(75)
+    t.left(90)
+    t.pen(pencolor=None)
+    t.forward(75)
+    t.left(90)
+    t.forward(75)
+
+def draw_g():
+    t.circle(50, -180) 
+    t.forward(100)  
+    t.right(90)
+    t.circle(50, 180)  
+    t.left(90)
+    t.forward(50) 
 
 # Letra I
 def draw_i():
